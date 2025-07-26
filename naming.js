833cefa62f97dcdfbdf5d6a20d29123628048a50
deluwarhoisn <babuhossen301@gmail.com>
2025-07-26 23:07:52 +0600

    added snake camel pascal case

---
+++ naming.js
@@ -0,0 +1,28 @@
+// 1. no keyword in variable name
+var false = 45;
+var var = 50;
+
+// 2. no space or gap in variable name
+var is happy = false;
+var ishappyes = true;
+
+// 3. no quote
+var "address" = "kochu khet";
+
+
+// 4. can not start with number. but number other than the first letter is alloweds
+var money = 45;
+var money2 = 55;
+var 3money = 88;
+
+
+// 5. name is case sensitive
+address 
+address
+address
+
+// 6. how to writ a long variable name
+var my current home address = "kharnoy";
+var my_current_home_address = "kharnoy";  //snake case    
+var myCurrentHomeAddress = "kharnoy"; //camel case
+var MyCurrentHomeAddress = "kharnoy"; //pascal case
