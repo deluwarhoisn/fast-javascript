482af2bdfd945d662038195e0482d5c4256b15cb
deluwarhoisn <babuhossen301@gmail.com>
2025-07-26 22:40:30 +0600

    added var and boolean

---
+++ types.js
@@ -0,0 +1,10 @@
+// string
+var name = "babu";
+console.log(name);
+var address = "kharnoy";
+var country = "bangladesh";
+// boolean
+var isRich = true;
+
+var passed = false;
+console.log(passed)
---
+++ variable.js
@@ -0,0 +1,11 @@
+/***
+ * VARIABLE==> vary + able
+ * 5 things to declare a variable 
+ */
+var weight = 38 ;
+var price = 38 ;
+var year = 199 ;
+var money = 2005 ;
+
+var age;
+console.log(money)
