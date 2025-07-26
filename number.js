31229933d0ba823189eba941638bcf20a352a077
deluwarhoisn <babuhossen301@gmail.com>
2025-07-26 23:33:51 +0600

    added parseint and parsefloat

---
+++ number.js
@@ -0,0 +1,12 @@
+var orangeprice = 20;
+var chocolate = 0.5;
+var appleprice = 25;
+
+
+console.log (typeof orangeprice);
+console.log(typeof appleprice);
+console.log(orangeprice + appleprice)
+ 
+
+var appleprice = parseInt('22.5')
+var appleprice = parseFloat('22.5');
