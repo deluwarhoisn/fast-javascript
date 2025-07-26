39c4bae835e3e5a22cc5183ddfaef7a023af7260
deluwarhoisn <babuhossen301@gmail.com>
2025-07-26 21:55:03 +0600

    added java script basic

---
+++ math.js
@@ -0,0 +1,17 @@
+/**
+ * ********************************
+ * MATH NEEDED FOR PROGRAMMING
+ * ********************************
+ * 1.NUMBER: +VE, -V [ 50, -9, 0]
+ * 2. INTEGER: 2, 88, [odd, even]: 2, 4, 897
+ * 3. float: 5.6, 2.2
+ * 4. operation: +, -, *, /
+ * 5. More oparation:%, []
+ * 6. 5%, 50 taka er 10% koto?
+ * 7. sum: 14, 18, 16
+ * 8. avg: 14, 18, 16
+ * 9. max/min: 14, 18, 16
+ * 10. series:
+ * 11. area: triangle
+ * 12. 
+ * / 
